b7f81ae67675acb5625cf9ca156ee2b1fc9a1e57
anna.pavanello <annaclaramorettipavanello@gmail.com>
2026-04-14 11:56:38 -0300

    componentes

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import ButtonChild from './components/ButtonChild.vue';
+</script>
+
+<template>
+<div class="container">
+  <div>
+    <button class="btn btn-novo">Novo</button>
+    <button class="btn btn-editar">Editar</button>
+    <button class="btn btn-salvar">Salvar</button>
+    <button class="btn btn-cancelar">Cancelar</button>
+    <button class="btn btn-excluir">Excluir</button>
+  </div>
+  <div>
+    <ButtonChild :nome="'Novo'" :tipo="'btn'" :acao="'btn-novo'"></ButtonChild>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.btn{
+  background-color: gray;
+  color: white;
+  border-radius: 5px;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  margin: 1px;
+}
+.btn-novo{
+  background-color: aqua;
+  color: black;
+}
+.btn-editar{
+  background-color: yellow;
+  color: black;
+}
+.btn-salvar{
+  background-color: green;
+}
+.btn-cancelar{
+  background-color: orange;
+}
+.btn-excluir{
+  background-color: red;
+}
+</style>
---
+++ src/components/ButtonChild.vue
@@ -0,0 +1,25 @@
+<script setup>
+    defineProps(['nome', 'tipo', 'acao'])
+</script>
+
+<template>
+    <button :class="[tipo, acao]">
+        {{ nome }}
+    </button>
+</template>
+
+<style scoped>
+.btn{
+  background-color: gray;
+  color: white;
+  border-radius: 5px;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  margin: 1px;
+}
+.btn-novo{
+  background-color: aqua;
+  color: black;
+}
+</style>
